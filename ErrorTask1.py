307197dbbb5b6fdd8812be2e3e4ee7d151a930b8
Nikita <mail@mail.ru>
2024-07-31 10:36:01 +0500

    Deleted

--- ErrorTask1.py
+++
@@ -1,8 +0,0 @@
-def calculate_average(numbers):
-    total = 0
-    for num in numbers:
-        total += num
-    return total / len(numbers)
-
-numbers = [1, 2, 3, 4, 5]
-print(calculate_average(numbers))
